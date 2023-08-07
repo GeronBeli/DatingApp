9e74cbd77296fd9f78f05761bfff6969a1d1c9b6
Geron Beli <geronbeli@gmail.com>
2023-08-07 19:51:21 +0200

    Add project files.

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using API.Entities;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,15 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+    }
+
+}

ca373a0971581beae7ca7531d2d7655719beba87
GeronBeli <geronbeli@gmail.com>
2025-06-21 17:15:37 +0200

    added create new user endpoint

--- Backend/app/routes/users.py
+++ Backend/app/routes/users.py
@@ -3,6 +3,9 @@ from starlette import status
 from ..database import db_dependency
 from sqlalchemy import select
 from ..models import Users
+from ..schemas.users import UserLogin
+from ..auth.service import bcrypt_context
+import os
 
 user_router = APIRouter(
     prefix='/user',
@@ -19,3 +22,19 @@ async def get_all_users(req: Request, db: db_dependency):
 async def get_user(id: int, db: db_dependency):
     user = db.scalar(select(Users).where(Users.id == id))
     return user
+
+
+@user_router.post("/")
+async def create_user(user: UserLogin, db: db_dependency):
+    salt = os.urandom(16).hex()
+
+    hashed_password = bcrypt_context.hash(user.password + salt)
+
+    new_user = Users(
+        username = user.username,
+        password_hash = hashed_password,
+        salt = salt
+    )
+
+    db.add(new_user)
+    db.commit()

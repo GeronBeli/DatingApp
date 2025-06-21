af7fde41dfa8fb940b526542a678db9855fdf39b
GeronBeli <geronbeli@gmail.com>
2025-06-21 02:08:17 +0200

    modified CORS

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -2,9 +2,17 @@ from fastapi import FastAPI
 from .models import Base
 from .database import engine
 from .routes.users import user_router
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
 
 
 Base.metadata.create_all(bind=engine)

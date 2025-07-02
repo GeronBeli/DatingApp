6fea738e7aa560047cb0c3a1859dae984f10bbd4
GeronBeli <geronbeli@gmail.com>
2025-07-02 17:28:39 +0200

    added login and register page

--- Frontend/dating-app/src/main.tsx
+++ Frontend/dating-app/src/main.tsx
@@ -1,11 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-// import App from './App.tsx'
-import DisplayUsers from './User.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import LoginPage from './components/Login.tsx'
+import RegisterPage from './components/Register.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <DisplayUsers />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
